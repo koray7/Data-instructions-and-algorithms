3ea4368d016ccc2ac2d39f5408e581d03888d360
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-04-09 19:08:41 -0500

    Routes Created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import './index.css'
-
+import './App.scss'
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import MovieDetail from './components/MovieDetail/MovieDetail';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
-    <>
-    <div className="App">
-    <h1>Movie App!!!</h1>
+
+    <div className="app">
+    
+    <Router>
+      <Header></Header>
+      <Routes>
+        
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:imdbID" element={<MovieDetail />} />
+          <Route element={<PageNotFound />} />
+          
+      </Routes>
+      <Footer />
+    </Router>
+
     </div>
-    </>
   );
 }
 
---
+++ src/common/apis/movieApiKey.js
@@ -0,0 +1,1 @@
+export const APIKey = "cba3f799";
