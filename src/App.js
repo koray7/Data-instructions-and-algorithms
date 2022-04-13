77c33a48e373974fdd755e34c55b1245d898cdb1
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-04-12 23:23:38 -0500

    Css updated

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
-import './App.scss'
+import './App.css'
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import MovieDetail from './components/MovieDetail/MovieDetail';
-import PageNotFound from './components/PageNotFound/PageNotFound';
-import Footer from './components/Footer/Footer'
+
 
 function App() {
   return (
 
     <div className="app">
-    
-    <Router>
-      <Header></Header>
-      <div className="container">
-
-      <Routes>
-        
-          <Route path="/" exact element={<Home />} />
-          <Route path="/movie/:imdbID" element={<MovieDetail />} />
-          <Route element={<PageNotFound />} />
-          
-      </Routes>
-      </div>
-      <Footer />
-    </Router>
+<h1>Hello App</h1>
 
     </div>
   );
