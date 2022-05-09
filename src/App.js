8c2a8d6657c6d52f2b0a139ff5a267e41be8f0b5
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-05-09 14:55:51 -0500

    Reset an Inherited Constructor Property

--- src/App.js
+++ src/App.js
@@ -794,10 +794,22 @@ function App() {
 // };
 
 
+// ######### Reset an Inherited Constructor Property  ############
 
 
 
+// function Animal() {}
+// function Bird() {}
+// function Dog() {}
 
+// Bird.prototype = Object.create(Animal.prototype);
+// Dog.prototype = Object.create(Animal.prototype);
+
+// Bird.prototype.constructor = Bird;
+// Dog.prototype.constructor = Dog;
+
+// let duck = new Bird();
+// let beagle = new Dog();
 
 
 
