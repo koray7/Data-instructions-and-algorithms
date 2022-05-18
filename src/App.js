12a9b5c6e03fe3b50d3e93bb51c60505186526f2
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-05-17 22:01:42 -0500

    Str reverse

--- src/App.js
+++ src/App.js
@@ -1007,8 +1007,19 @@ function App() {
 
 
 
+// #######  String reverse ######
 
+// function reverse(str){
+//   var rtnStr = '';
+//   for(var i = str.length-1; i>=0;i--){
+//     rtnStr +=str[i];
+//   }
+//   return rtnStr;
+// }
 
+// > reverse('you are a nice dude');
+//   = "edud ecin a era uoy"
+        
 
 
 
