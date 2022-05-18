34d6c75c68cf3f16f53465e4fe67583cff2aa326
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-05-17 22:03:07 -0500

    Sum of Two

--- src/App.js
+++ src/App.js
@@ -1021,6 +1021,37 @@ function App() {
 //   = "edud ecin a era uoy"
         
 
+// ##### SUM OF TWO #############
+
+// function sumFinder(arr, sum){
+//   var len = arr.length;
+  
+//   for(var i =0; i<len-1; i++){  
+//      for(var j = i+1;j<len; j++){
+//         if (arr[i] + arr[j] == sum)
+//             return true;
+//      }
+//   }
+  
+//   return false;
+// }
+
+// > sumFinder([6,4,3,2,1,7], 9);
+//   = true
+// > sumFinder([6,4,3,2,1,7], 2);
+//   = false
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
