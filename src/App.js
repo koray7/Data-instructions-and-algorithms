cf2178e1055468311096c4500d20b02a5f178a95
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-05-24 11:26:13 -0500

    Refs in React

--- src/App.js
+++ src/App.js
@@ -1199,12 +1199,34 @@ function App() {
 // }
 
 
+// ####  Refs in React ########
+
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.myRef = React.createRef();
+//   }
+//   render() {
+//     return <div ref={this.myRef} />;
+//   }
+// }
 
 
-
-
-
-
+// class UserForm extends Component {
+//   handleSubmit = () => {
+//     console.log("Input Value is: ", this.input.value)
+//   }
+//   render () {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <input
+//           type='text'
+//           ref={(input) => this.input = input} /> // Access DOM input in handle submit
+//         <button type='submit'>Submit</button>
+//       </form>
+//     )
+//   }
+// }
 
 
 
