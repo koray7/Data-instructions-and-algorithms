17e15a9d52358f9168ab8ec30fa8ea5faf86fabd
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-05-24 11:28:56 -0500

    Context API React

--- src/App.js
+++ src/App.js
@@ -1229,6 +1229,45 @@ function App() {
 // }
 
 
+// ###### Context API in React #########
+
+
+// const ThemeContext = React.createContext('light');
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <ThemeContext.Provider value="dark">
+//         <Toolbar />
+//       </ThemeContext.Provider>
+//     );
+//   }
+// }
+
+// function Toolbar() {
+//   return (
+//     <div>
+//       <ThemedButton />
+//     </div>
+//   );
+// }
+// class ThemedButton extends React.Component {
+//   static contextType = ThemeContext;
+//   render() {
+//     return <Button theme={this.context} />;
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
