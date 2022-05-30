e4d5813d4b79c5328581060382bda296c1d4cae7
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-05-30 17:59:59 -0500

    Override Inherited Methods

--- src/App.js
+++ src/App.js
@@ -1284,9 +1284,24 @@ function App() {
 // beagle.bark();
 
 
+// ###########################       ##############
+
+// function Bird() { }
+
+// Bird.prototype.fly = function() { return "I am flying!"; };
+
+// function Penguin() { }
+// Penguin.prototype = Object.create(Bird.prototype);
+// Penguin.prototype.constructor = Penguin;
 
 
+// Penguin.prototype.fly = function() {
+//   return "Alas, this is a flightless bird.";
+// };
 
+
+// let penguin = new Penguin();
+// console.log(penguin.fly());
 
 
 
