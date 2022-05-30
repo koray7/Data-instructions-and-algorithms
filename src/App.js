95fc53192d477c313b4db8a15b455007603e38c3
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-05-30 17:58:21 -0500

    Add Methods After Inheritance

--- src/App.js
+++ src/App.js
@@ -1262,6 +1262,30 @@ function App() {
 
 
 
+// ########### Add Methods After Inheritance #########
+// function Animal() {}
+// Animal.prototype.eat = function() {
+//   console.log("nom nom nom");
+// };
+
+// function Dog() {}
+
+
+// Dog.prototype = Object.create(Animal.prototype);
+// Dog.prototype.constructor = Dog;
+// Dog.prototype.bark = function() {
+//   console.log("Woof!");
+// };
+
+
+// let beagle = new Dog();
+
+// beagle.eat();
+// beagle.bark();
+
+
+
+
 
 
 
