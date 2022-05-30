55f6e431c268537e5408c2c036b05193085fd600
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-05-30 18:09:43 -0500

    Functional Programming Terminology

--- src/App.js
+++ src/App.js
@@ -1284,7 +1284,7 @@ function App() {
 // beagle.bark();
 
 
-// ###########################       ##############
+// ###########################   Override Inherited Methods    ##############
 
 // function Bird() { }
 
@@ -1304,7 +1304,31 @@ function App() {
 // console.log(penguin.fly());
 
 
+// ############ Functional Programming Terminology   #####################
 
+
+// const prepareGreenTea = () => 'greenTea';
+// const prepareBlackTea = () => 'blackTea';
+
+// const getTea = (prepareTea, numOfCups) => {
+//   const teaCups = [];
+
+//   for(let cups = 1; cups <= numOfCups; cups += 1) {
+//     const teaCup = prepareTea();
+//     teaCups.push(teaCup);
+//   }
+//   return teaCups;
+// };
+
+// // Only change code below this line
+// const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+// const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
+// // Only change code above this line
+
+// console.log(
+//   tea4GreenTeamFCC,
+//   tea4BlackTeamFCC
+// );
 
 
 
