a11f74388d28765e52e10caf9c5cf32a6f8ca77b
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-06-19 11:41:23 -0500

    Slice() Method

--- src/App.js
+++ src/App.js
@@ -1384,6 +1384,13 @@ function App() {
 // console.log(usersUnder30);
 
 
+// ##### SLICE() #####
+
+// const arr = ["Cat", "Dog", "Tiger", "Zebra"];
+// const newArray = arr.slice(1, 3);
+
+
+
 
 
 
