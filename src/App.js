b697cc4c207829700f0a3b608035d6420f2f79e3
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-06-20 16:44:56 -0500

    join Method

--- src/App.js
+++ src/App.js
@@ -1457,11 +1457,19 @@ function App() {
 
 
 
+// ###### Combine an Array into a String Using the join Method ####
 
 
 
+// function sentensify(str) {
+
+//  return str.split(/\W/).join(" ");
+// }
+
+// sentensify("May-the-force-be-with-you");
 
 
+// [May the force be with you]
 
 
 
