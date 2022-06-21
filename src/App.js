9667a5e157c5757a18c9760f3325a429c78f5ead
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-06-21 12:27:03 -0500

    Seek and Destroy

--- src/App.js
+++ src/App.js
@@ -1517,6 +1517,48 @@ function App() {
 
 
 
+// ####### Seek and Destroy ######
+
+// function destroyer(arr) {
+//   const valsToRemove = Object.values(arguments).slice(1);
+//   const filteredArray = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     let removeElement = false;
+//     for (let j = 0; j < valsToRemove.length; j++) {
+//       if (arr[i] === valsToRemove[j]) {
+//         removeElement = true;
+//       }
+//     }
+//     if (!removeElement) {
+//       filteredArray.push(arr[i]);
+//     }
+//   }
+//   return filteredArray;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
