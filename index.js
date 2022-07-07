d8d30adf9c382de3a74392f415f5b2ce0ee5ba33
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-07-07 17:42:49 -0500

    Truth check

--- index.js
+++ index.js
@@ -48,3 +48,40 @@
 //   dropElements([1, 2, 3, 4], function(n) {
 //     return n >= 3;
 //   });
+// function binaryAgent(str) {
+//     var biString = str.split(" ");
+//     var uniString = [];
+
+//     /*using the radix (or base) parameter in parseInt, we can convert the binary
+//         number to a decimal number while simultaneously converting to a char*/
+
+//     for (var i = 0; i < biString.length; i++) {
+//       uniString.push(String.fromCharCode(parseInt(biString[i], 2)));
+//     }
+
+//     return uniString.join("");
+//   }
+
+//   binaryAgent(
+//     "01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"
+//   );
+function truthCheck(collection, pre) {
+  let counter = 0;
+
+  for (let c in collection) {
+    if (collection[c].hasOwnProperty(pre) && Boolean(collection[c][pre])) {
+      counter++;
+    }
+  }
+
+  return counter == collection.length;
+}
+
+truthCheck(
+  [
+    { name: "Quincy", role: "Founder", isBot: false },
+    { name: "Naomi", role: "", isBot: false },
+    { name: "Camperbot", role: "Bot", isBot: true },
+  ],
+  "isBot"
+);
